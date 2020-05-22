317bbbebacf0b6d3ac4a6daabdfa70d3e25eee4e
Lukashev <lnv161297@gmail.com>
2020-05-22 14:05:33 +0300

    add cart modal, implement cart clear & add

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,7 +1,53 @@
 import React from "react"
+import { ContextApp } from "../store/reducer"
+import Grid from "@material-ui/core/Grid"
+import { Typography, IconButton } from "@material-ui/core"
+import DeleteIcon from "@material-ui/icons/Delete"
+import { useStyles } from "../styles/common"
 
-const CartItem = () => {
-  return <div></div>
+const CartItem = (props) => {
+  const classes = useStyles()
+  const { id, title, description, image, price } = props
+  const {
+    state: { currency },
+    dispatch,
+  } = React.useContext(ContextApp)
+
+  const deleteFromCart = () => {
+    dispatch({ type: "DELETE_PRODUCT_FROM_CART", payload: id })
+  }
+
+  return (
+    <Grid
+      container
+      justify="space-around"
+      alignItems="center"
+      direction="row"
+      className={classes.spaceBottom}
+    >
+      <Grid item xs={2}>
+        <img src={image} width={100} height={100} alt={title} />
+      </Grid>
+      <Grid item xs={3}>
+        <Typography variant="h6">{title}</Typography>
+      </Grid>
+      <Grid item xs={4}>
+        <Typography variant="body2" component="p">
+          {description}
+        </Typography>
+      </Grid>
+      <Grid item xs={2} className={classes.textCenter}>
+        <Typography variant="subtitle1">
+          {currency === "USA" ? `${price}$` : `${Math.round(price * 0.92)}€`}
+        </Typography>
+      </Grid>
+      <Grid item xs={1}>
+        <IconButton onClick={deleteFromCart}>
+          <DeleteIcon color="secondary" />
+        </IconButton>
+      </Grid>
+    </Grid>
+  )
 }
 
 export default CartItem
--- src/components/CartModal.jsx
+++ src/components/CartModal.jsx
@@ -6,6 +6,8 @@ import Modal from "@material-ui/core/Modal"
 import Backdrop from "@material-ui/core/Backdrop"
 import Fade from "@material-ui/core/Fade"
 import CartItem from "./CartItem"
+import { useStyles as useCommonStyles } from "../styles/common"
+import { Typography, Button } from "@material-ui/core"
 
 const useStyles = makeStyles((theme) => ({
   modal: {
@@ -21,8 +23,8 @@ const useStyles = makeStyles((theme) => ({
     padding: theme.spacing(2, 4, 3),
   },
   productContainer: {
+    overflowX: "auto",
     height: 500,
-    overflowY: "auto",
   },
 }))
 
@@ -30,11 +32,19 @@ const CartModal = () => {
   const handleClose = () => {
     dispatch({ type: "SET_CART_MODAL_VISIBLE", payload: false })
   }
+  const clearCart = () => dispatch({ type: "CLEAR_CART" })
   const {
     dispatch,
-    state: { cartModalVisible, cart },
+    state: { cartModalVisible, cart, currency },
   } = React.useContext(ContextApp)
+
+  const commonStyles = useCommonStyles()
   const classes = useStyles()
+  const calcSum = React.useMemo(
+    () => cart.reduce((acc, product) => acc + product.price, 0),
+    [cart]
+  )
+
   return (
     <Modal
       aria-labelledby="transition-modal-title"
@@ -49,12 +59,45 @@ const CartModal = () => {
       }}
     >
       <Fade in={cartModalVisible}>
-        <Grid container className={classes.paper}>
-          <h2 id="transition-modal-title">React Pizza Cart</h2>
-          <Grid container className={classes.productContainer}>
-            {cart.map((product) => (
-              <CartItem key={product.id} {...product} />
-            ))}
+        <Grid container className={classes.paper} direction="column">
+          <Grid container alignItems="center" justify="space-between">
+            <Grid md={6} item>
+              <Typography variant="h5" className={commonStyles.spaceBottom}>
+                React Pizza Cart
+              </Typography>
+            </Grid>
+            <Grid
+              md={6}
+              container
+              item
+              justify="space-between"
+              alignItems="center"
+            >
+              <Button color="secondary" onClick={clearCart}>
+                Clear
+              </Button>
+              <Button colr="primary">Checkout</Button>
+              <Typography variant="h6">{`Total: ${
+                currency === "USA"
+                  ? `${calcSum}$`
+                  : `${Math.round(calcSum * 0.92)}€`
+              }`}</Typography>
+            </Grid>
+          </Grid>
+          <Grid
+            container
+            className={classes.productContainer}
+            direction="column"
+          >
+            {cart.length ? (
+              cart.map((product, index) => (
+                <CartItem key={`${product.id}${index}`} {...product} />
+              ))
+            ) : (
+              <Typography variant="body2">
+                Cart is empty. Please, add some products.
+              </Typography>
+            )}
           </Grid>
         </Grid>
       </Fade>
